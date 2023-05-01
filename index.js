73a5624ce0f374a7ea8f1cbfe82dd94fe960992a
Alphonse Johnson <a.joh26@gmail.com>
2023-05-01 16:14:13 -0500

    added example code for triangle and circle

--- index.js
+++ index.js
@@ -1,45 +1,60 @@
 const shape = require('./lib/shapes')
 const fs = require('fs');
-const inquire = require('inquirer');
-
-const questions =[
-
-    {
-
-    },
-    {
-    type: "input",
-    name: "stroke",
-    message: "What is the text color"
-
-    },
-    {
-    type: "list",
-    name:"shape",
-    message: "What shape will you choose",
-    choices: ['triangle', 'circle', 'square']
-
-    },
-    {type: "input",
-    name: "shapecolor",
-    message: "What is the color of the shape"
-
-    }
+const inquirer = require('inquirer');
 
 
-]
 
-function runPrompt (){
-    return inquirer.prompt(questions)
-        .then ((answers) => {
-            const info = generateMarkdown(answers)
-            fs.writeFile('examples', info, (err) => {
-                err ? console.error(err) : console.log ('Success today') 
+// const questions =[
+
+//     {
+//     type: "input",
+//     name: "title",
+//     message: "What is the title of your project?",
+
+//     },
+
+//     {
+//     type: "input",
+//     name: "stroke",
+//     message: "Enter comma seperate rgb values for the text color"
+//     },
+
+//     {
+//     type: "list",
+//     name:"shape",
+//     message: "What shape will you choose",
+//     choices: ['triangle', 'circle', 'square']
+//     },
+
+//     {
+//     type: "input",
+//     name: "shapecolor",
+//     message: "Enter comma seperate rgb values for the shape color",
+//     },
+// ]
+
+const svg = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+<polygon points="50,0 50,100 0,100" />
+</svg>`;
+
+// function runPrompt (){
+//     return inquirer.prompt(questions)
+//         .then ((answers) => {
+//             const info = new shape(answers)
+            fs.writeFile("logo.svg", svg, function (err) {
+                if (err) console.log(err);
         }
 
 
 )
+
+
+fs.writeFile("logo2.svg", shape(), function (err) {
+    if (err) console.log(err);
 }
-   )}
-   runPrompt();
+)
+// }
+//    )}
+
+//    runPrompt();
 
