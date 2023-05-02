25d7d95513711cabf3c2060dbedd67cc3bce5cfb
Alphonse Johnson <a.joh26@gmail.com>
2023-05-02 10:34:00 -0500

    moved drawing functionality to shapes file, added console log message and logic for input validation and function

--- index.js
+++ index.js
@@ -1,88 +1,67 @@
-const shapeChoice = require('./lib/shapes')
-const fs = require('fs');
-const inquirer = require('inquirer');
+const drawShape = require("./lib/shapes");
+
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 
-
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-      type: 'input',
-      name: 'fillcolor',
-      message: 'Enter the color of the shape:',
-      validate:  function (value){
+      type: "input",
+      name: "fillcolor",
+      message: "Enter the color of the shape:",
+      validate: function (value) {
         let hex = /^#[0-9A-F]{6}$/i.test(value);
-            if (hex){
-                return true;
-            }else {
-                return 'Please enter a valid hexidecimal color code.';
-                }
-            },   
-      }   
-     
-    
-    ,
-    {
-      type: 'input',
-      name: 'strokecolor',
-      message: 'Enter the color for the stroke:',
-      validate: function(value){
-        let hex = /^#[0-9A-F]{6}$/i.test(value);
-            if (hex){
-                return true;
-            }else {
-                return 'Please enter a valid hexidecimal color code.';
-                }
-            }
-    },
-    {
-    type: 'list',
-    name: 'choice',
-    message: 'Choose your shape:',
-    choices: ['triangle', 'circle', 'square']
+        let vcolor = "blue";
+        if (hex || vcolor) {
+          return true;
+        } else {
+          return "Please enter a valid hexidecimal color code.";
+        }
+      },
     },
 
-  ]).then((answers) => {
-    const { fillcolor, strokecolor, choice } = answers;
-    const drawTriangle = `<svg height="100" width="100">
-                  <polygon points="50,0 0,100 100,100" stroke="${strokecolor}" fill="${fillcolor}" ;stroke-width:1;stroke:"5" />
-                </svg>`;
+    {
+      type: "input",
+      name: "strokecolor",
+      message: "Enter the color for the stroke:",
+      validate: function (value) {
+        let hex = /^#[0-9A-F]{6}$/i.test(value);
+        let vcolor = "green";
+        if (hex || vcolor) {
+          return true;
+        } else {
+          return "Please enter a valid hexidecimal color code.";
+        }
+      },
+    },
+    {
+      type: "list",
+      name: "choice",
+      message: "Choose your shape:",
+      choices: ["triangle", "circle", "square"],
+    },
+  ])
+  .then((answers) => {
+    const { choice } = answers;
+    const info = drawShape(answers);
 
-    
-    const drawCircle=`
-    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="25" cy="75" r="20" stroke="${strokecolor}" fill="${fillcolor}" stroke-width="5"/>
-    </svg>`;
-
-    const drawSquare =`
-    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
-
-    <rect x="10" y="10" width="30" height="30" stroke="${strokecolor}" fill="${fillcolor}" stroke-width="5"/>
-    
-    `;
-
-
-    if(choice === 'triangle'){
-    fs.writeFile('./examples/triangle.svg', drawTriangle, (err) => {
-      if (err) throw err;
-    }
-    )
-    }else if(choice === 'circle'){
-        fs.writeFile('./examples/circle.svg', drawCircle, (err) => {
-            if (err) throw err; 
-          }
-          )    
-    }else
-    fs.writeFile('./examples/square.svg', drawSquare, (err) => {
-        if (err) throw err; 
-      }
-      )   
-    
-    ; 
-  }
-  );
-
-
-
-
-
-
+    if (choice === "triangle") {
+      fs.writeFile("./examples/logo.svg", info, (err) => {
+        if (err) throw err;
+        else
+        console.log("Generated logo.svg")
+      });
+    } else if (choice === "circle") {
+      fs.writeFile("./examples/logo.svg", info, (err) => {
+        if (err) throw err;
+        else
+        console.log("Generated logo.svg")
+      });
+    } else
+      fs.writeFile("./examples/logo.svg", info, (err) => {
+        if (err) throw err;
+        else
+        console.log("Generated logo.svg")
+      });
+  });
