162b8546f41d356421caf001fe761b4c081f00a6
Alphonse Johnson <a.joh26@gmail.com>
2023-05-01 23:18:08 -0500

    test out pathing for output

--- index.js
+++ index.js
@@ -7,13 +7,32 @@ const inquirer = require('inquirer');
 inquirer.prompt([
     {
       type: 'input',
-      name: 'color',
-      message: 'Enter the color of the triangle:',
-    },
+      name: 'fillcolor',
+      message: 'Enter the color of the shape:',
+      validate:  function (value){
+        let hex = /^#[0-9A-F]{6}$/i.test(value);
+            if (hex){
+                return true;
+            }else {
+                return 'Please enter a valid hexidecimal color code.';
+                }
+            },   
+      }   
+     
+    
+    ,
     {
       type: 'input',
-      name: 'rgb',
-      message: 'Enter the RGB value of the color:',
+      name: 'strokecolor',
+      message: 'Enter the color for the stroke:',
+      validate: function(value){
+        let hex = /^#[0-9A-F]{6}$/i.test(value);
+            if (hex){
+                return true;
+            }else {
+                return 'Please enter a valid hexidecimal color code.';
+                }
+            }
     },
     {
     type: 'list',
@@ -23,37 +42,37 @@ inquirer.prompt([
     },
 
   ]).then((answers) => {
-    const { color, rgb, choice } = answers;
+    const { fillcolor, strokecolor, choice } = answers;
     const drawTriangle = `<svg height="100" width="100">
-                  <polygon points="50,0 0,100 100,100" style="fill:${color};stroke-width:1;stroke:${rgb}" />
+                  <polygon points="50,0 0,100 100,100" stroke="${strokecolor}" fill="${fillcolor}" ;stroke-width:1;stroke:"5" />
                 </svg>`;
 
     
     const drawCircle=`
     <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="25" cy="75" r="20" stroke="red" fill="${color}" stroke-width="${rgb}"/>
+    <circle cx="25" cy="75" r="20" stroke="${strokecolor}" fill="${fillcolor}" stroke-width="5"/>
     </svg>`;
 
     const drawSquare =`
     <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
 
-    <rect x="10" y="10" width="30" height="30" stroke="black" fill="${color}" stroke-width="${rgb}"/>
+    <rect x="10" y="10" width="30" height="30" stroke="${strokecolor}" fill="${fillcolor}" stroke-width="5"/>
     
     `;
 
 
     if(choice === 'triangle'){
-    fs.writeFile('triangle.svg', drawTriangle, (err) => {
+    fs.writeFile('./examples/triangle.svg', drawTriangle, (err) => {
       if (err) throw err;
     }
     )
     }else if(choice === 'circle'){
-        fs.writeFile('circle.svg', drawCircle, (err) => {
+        fs.writeFile('./examples/circle.svg', drawCircle, (err) => {
             if (err) throw err; 
           }
           )    
     }else
-    fs.writeFile('square.svg', drawSquare, (err) => {
+    fs.writeFile('./examples/square.svg', drawSquare, (err) => {
         if (err) throw err; 
       }
       )   
