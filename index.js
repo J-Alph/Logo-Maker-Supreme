e9d5f794092b2e01f3d239e94cacf29b5c3566c4
Alphonse Johnson <a.joh26@gmail.com>
2023-05-01 22:02:09 -0500

    Added code to allow for choices for shapes

--- index.js
+++ index.js
@@ -1,60 +1,69 @@
-const shape = require('./lib/shapes')
+const shapeChoice = require('./lib/shapes')
 const fs = require('fs');
 const inquirer = require('inquirer');
 
 
 
-// const questions =[
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter the color of the triangle:',
+    },
+    {
+      type: 'input',
+      name: 'rgb',
+      message: 'Enter the RGB value of the color:',
+    },
+    {
+    type: 'list',
+    name: 'choice',
+    message: 'Choose your shape:',
+    choices: ['triangle', 'circle', 'square']
+    },
 
-//     {
-//     type: "input",
-//     name: "title",
-//     message: "What is the title of your project?",
+  ]).then((answers) => {
+    const { color, rgb, choice } = answers;
+    const drawTriangle = `<svg height="100" width="100">
+                  <polygon points="50,0 0,100 100,100" style="fill:${color};stroke-width:1;stroke:${rgb}" />
+                </svg>`;
 
-//     },
+    
+    const drawCircle=`
+    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="25" cy="75" r="20" stroke="red" fill="${color}" stroke-width="${rgb}"/>
+    </svg>`;
 
-//     {
-//     type: "input",
-//     name: "stroke",
-//     message: "Enter comma seperate rgb values for the text color"
-//     },
+    const drawSquare =`
+    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
 
-//     {
-//     type: "list",
-//     name:"shape",
-//     message: "What shape will you choose",
-//     choices: ['triangle', 'circle', 'square']
-//     },
-
-//     {
-//     type: "input",
-//     name: "shapecolor",
-//     message: "Enter comma seperate rgb values for the shape color",
-//     },
-// ]
-
-const svg = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-<polygon points="50,0 50,100 0,100" />
-</svg>`;
-
-// function runPrompt (){
-//     return inquirer.prompt(questions)
-//         .then ((answers) => {
-//             const info = new shape(answers)
-            fs.writeFile("logo.svg", svg, function (err) {
-                if (err) console.log(err);
-        }
+    <rect x="10" y="10" width="30" height="30" stroke="black" fill="${color}" stroke-width="${rgb}"/>
+    
+    `;
 
 
-)
+    if(choice === 'triangle'){
+    fs.writeFile('triangle.svg', drawTriangle, (err) => {
+      if (err) throw err;
+    }
+    )
+    }else if(choice === 'circle'){
+        fs.writeFile('circle.svg', drawCircle, (err) => {
+            if (err) throw err; 
+          }
+          )    
+    }else
+    fs.writeFile('square.svg', drawSquare, (err) => {
+        if (err) throw err; 
+      }
+      )   
+    
+    ; 
+  }
+  );
 
 
-fs.writeFile("logo2.svg", shape(), function (err) {
-    if (err) console.log(err);
-}
-)
-// }
-//    )}
 
-//    runPrompt();
+
+
 
